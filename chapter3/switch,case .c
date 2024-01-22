4d425b4fe47d690ca1a042ea4ca0fab814ff83ed
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-01-22 22:43:31 +0545

    hey this is direct from the terminal

--- chapter3/switch,case .c
+++ chapter3/switch,case .c
@@ -29,11 +29,71 @@ break;
  }
     return 0;
 }
+
+
 // this is a line for comment 
 #include<stdio.h>
 int main()
 { 
-  int 
+  int minority;
+  char minor;
+  char answer[2];
+  printf("enter y for yes n for no with age ");
+  scanf("%c %d",&minor,&minority);
+  if (minor=='y' && minority<=17)
+  {
+    switch (minor)
+    {
+    case 16:
+    // so i have used if age==16 then print call me but using switch statement we can make case for all the ages as of mine there would be form 17 to 1 so we can have 17 cases is we wanted ...
+    {
+      printf("call me \n")
+    }
+      break;
+    case 15:
+    {
+      printf("contact me\n ")
+    } 
+    break;
+    // the case 14 act like if age 14 so we can use switch indeed...
+    case:14 
+    {
+      printf("do you know one thing \n");
+      printf("chose a or b \n");
+      scanf("%c",&answer);
+    //   if (answer=='a')
+    //   {
+    //     printf("dont know what you been looking for \n")
+    //   }
+      
+    // }
+    // break;
+    // case 4:if (answer=='b')
+
+    // {
+    //   printf("i remember when i first\n ")
+    // }
+    switch (answer[0])
+    {
+    case 'a':
+    printf("you dont know why you been looking for ");
+
+      break;
+    case 'b':
+    printf("i remember when i first .....");
+    break;
+    default:
+    printf("invalid choice");
+      break;  
+      }
+    break;
+    default:printf("thank for talking kid\n ");
+      break;
+    }
+  }
+  else{
+    printf("come again ");
+  }
   return 0;
 }
 
