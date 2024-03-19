36f30f556ad0d9450c7d7d4670734f3bff3fe7b1
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-03-19 14:40:27 +0545

    strings

---
+++ chapter 8/string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{      // things to remember that  theres a null character at the end of the string by itself we dont have to  initialize it and we can also initialize  by our self
+
+     // it takes up a values in memory and  have its adress in a variable ......
+     
+    char str [] = {'d','h','i','r','e','n','\0'};
+    char str [] ="dhirendra"; // in this the null character is already written 
+
+// Both of the str var prints the same values and its the easier way to print and store values in  a variables....
+
+
+
+    return 0;
+}
