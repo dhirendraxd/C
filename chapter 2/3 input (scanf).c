ceece562cf3db9a28f89902275b0fbcb7709fc01
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-04-11 16:49:42 +0545

    new changes

--- chapter 2/3 input (scanf).c
+++ chapter 2/3 input (scanf).c
@@ -9,7 +9,7 @@ int  main(){
     // the & is used to allocate the location of where the input should be stored  after the user gives the data in the above case it should be stored in a and the  format specifier  if %d cause it is and integer to be converted and stored in  (variable:a)
     printf("enter the value of b \n");
     // after a printf an scanf should be written to get the input from user 
-    scanf("%s", &b)
+    scanf("%s", &b);
      // in this line of code the scanf take the character value of b  entered by user and assign it to variable b and %s is used to read the   string of characters 
 printf("the sum of a and b is %d ", a+b);
     return 0;
