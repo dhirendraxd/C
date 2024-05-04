02fd7324d5e18137b13f5a979d497b3f25584a39
dhirendra singh dhami <dhiren@kali.Dhirendra>
2024-05-04 21:58:01 +0545

    new changes

---
+++ REVISION/7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    
+    int x=43;
+    int y=53;
+    int z=50;
+    z=x&y;
+      z=x|y;
+        z=x^y;
+          z=x>>y;
+            z=<<y;
+      printf("%d",z);
+        printf("%d",z);        
+          printf("%d",z);
+            printf("%d",z);
+return 0;
+}
+// dma 
